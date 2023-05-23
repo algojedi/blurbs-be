a83b550c350a4bae06c7226078ecc3b104a7b4ce
Sukhdev <48499132+algojedi@users.noreply.github.com>
2023-05-22 21:22:08 -0400

    added mockito and junit5 and tested post req to create new post

--- src/test/java/ca/sheridancollege/banwsukh/web/rest/PostControllerTest.java
+++ src/test/java/ca/sheridancollege/banwsukh/web/rest/PostControllerTest.java
@@ -1,45 +1,72 @@
 package ca.sheridancollege.banwsukh.web.rest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import ca.sheridancollege.banwsukh.beans.PostReq;
 import ca.sheridancollege.banwsukh.domain.AppUser;
 import ca.sheridancollege.banwsukh.domain.Post;
-import ca.sheridancollege.banwsukh.services.AppUserServiceImpl;
+import ca.sheridancollege.banwsukh.services.AppUserService;
 import ca.sheridancollege.banwsukh.services.PostService;
 
+@ExtendWith(MockitoExtension.class)
 class PostControllerTest {
-    
-		@Autowired
-	    private PostService postService;
 
-		@Autowired
-	    private AppUserServiceImpl appUserService;
+    @Mock
+    private PostService postService;
 
-//	    private PostController postController;
+    @Mock
+    private AppUserService appUserService;
 
-	    @Test
-	    void testAddPost() {
-	    	  PostController postController = new PostController(postService, appUserService);
-	          PostReq postReq = new PostReq();
-	          postReq.setUserId(1L);
-	          postReq.setTitle("Test Post");
-	          postReq.setContent("Just some content");
-	          AppUser user = new AppUser();
-	          user.setId(1L);
-//	          Mockito.when(appUserService.findById(1L)).thenReturn(user);
-//	          Mockito.when(postService.save(Mockito.any(Post.class))).thenReturn(new Post());
+    @InjectMocks
+    private PostController postController;
 
-	          // Act
-	          System.out.println(postController.toString());
-	          ResponseEntity<Post> response = postController.addPost(postReq);
+//    @Before
 
-	          // Assert
-	          assert response.getStatusCode() == HttpStatus.OK;
-	          assert response.getBody() != null;
-	    }
+	@Test
+	public void testAddPost() {
+		// Mock data
+		PostReq postReq = new PostReq();
+		postReq.setUserId(1L);
+		postReq.setTitle("Test Title");
+		postReq.setContent("Test Content");
+
+		// Mock behavior of appUserService.findById()
+		AppUser user = new AppUser();
+		user.setId(1L);
+		Optional<AppUser> userOptional = Optional.of(user);
+
+		when(appUserService.findById(1L)).thenReturn(userOptional);
+
+		// Mock behavior of postService.save()
+		Post savedPost = new Post();
+		savedPost.setId(1L);
+		when(postService.save(any(Post.class))).thenReturn(savedPost);
+
+		// Call the method under test
+		ResponseEntity<Post> response = postController.addPost(postReq);
+
+		// Verify the expected behavior
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(savedPost, response.getBody());
+
+		// Verify that appUserService.findById() was called with the correct argument
+		verify(appUserService).findById(postReq.getUserId());
+
+		// Verify that postService.save() was called with the correct argument
+		verify(postService).save(any(Post.class));
+	}
 }
