6e4aea04e86bb7aef67dfa0ee5707cd579afb65d
Sukhdev <48499132+algojedi@users.noreply.github.com>
2023-05-28 19:45:34 -0400

    more seed data

--- src/main/java/ca/sheridancollege/banwsukh/bootstrap/PostBootstrap.java
+++ src/main/java/ca/sheridancollege/banwsukh/bootstrap/PostBootstrap.java
@@ -35,43 +35,88 @@ public class PostBootstrap implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		// Create instances of AppUser and set their properties
-		AppUser johnLennon = AppUser.create("John Lennon", "test");
-		AppUser paulMcCartney = AppUser.create("Paul McCartney", "test");
-		AppUser leonardCohen = AppUser.create("Leonard Cohen", "test");
+	    // Create instances of AppUser and set their properties
+	    AppUser johnLennon = AppUser.create("John Lennon", "test");
+	    AppUser paulMcCartney = AppUser.create("Paul McCartney", "test");
+	    AppUser leonardCohen = AppUser.create("Leonard Cohen", "test");
+	    AppUser bobDylan = AppUser.create("Bob Dylan", "test");
+	    AppUser davidBowie = AppUser.create("David Bowie", "test");
 
-		// Create instances of Post and set their properties
-		Post p1 = new Post();
-		p1.setTitle("All You Need is Love");
-		p1.setContent("There's nothing you can sing that can't be sung...");
+	    // Save the AppUser objects using the appUserService.save() method
+	    appUserService.save(johnLennon);
+	    appUserService.save(paulMcCartney);
+	    appUserService.save(leonardCohen);
+	    appUserService.save(bobDylan);
+	    appUserService.save(davidBowie);
 
-		Post p2 = new Post();
-		p2.setTitle("Let it Be");
-		p2.setContent("When I find myself in times of trouble...");
+	    // Create instances of Post and set their properties
+	    Post p1 = new Post();
+	    p1.setTitle("All You Need is Love");
+	    p1.setContent("There's nothing you can sing that can't be sung...");
+	    p1.setAppUser(johnLennon);
 
-		Post p3 = new Post();
-		p3.setTitle("Hallelujah");
-		p3.setContent("Now I've heard there was a secret chord...");
+	    Post p2 = new Post();
+	    p2.setTitle("Let it Be");
+	    p2.setContent("When I find myself in times of trouble...");
+	    p2.setAppUser(paulMcCartney);
 
-		// Assign the retrieved AppUser objects to the AppUser property of each Post
-		appUserService.save(johnLennon);
-		appUserService.save(paulMcCartney);
-		appUserService.save(leonardCohen);
-		p1.setAppUser(johnLennon);
-		p2.setAppUser(paulMcCartney);
-		p3.setAppUser(leonardCohen);
+	    Post p3 = new Post();
+	    p3.setTitle("Hallelujah");
+	    p3.setContent("Now I've heard there was a secret chord...");
+	    p3.setAppUser(leonardCohen);
 
-		// Save the Post objects using the postService.save() method
-		p1 = postService.save(p1);
-		p2 = postService.save(p2);
-		p3 = postService.save(p3);
+	    Post p4 = new Post();
+	    p4.setTitle("Blowin' in the Wind");
+	    p4.setContent("How many roads must a man walk down...");
+	    p4.setAppUser(bobDylan);
 
-		// Save ratings
-		AppUserPostRating userPostRating = new AppUserPostRating();
-		userPostRating.setPost(p1);
-		userPostRating.setUser(leonardCohen);
-		userPostRating.setRating(4.5);
-		userPostRating = appUserPostRatingService.save(userPostRating);
-		postService.updateAverageRating(p1);
+	    Post p5 = new Post();
+	    p5.setTitle("Space Oddity");
+	    p5.setContent("Ground Control to Major Tom...");
+	    p5.setAppUser(davidBowie);
+
+	    // Save the Post objects using the postService.save() method
+	    p1 = postService.save(p1);
+	    p2 = postService.save(p2);
+	    p3 = postService.save(p3);
+	    p4 = postService.save(p4);
+	    p5 = postService.save(p5);
+
+	    // Save ratings
+	    AppUserPostRating userPostRating1 = new AppUserPostRating();
+	    userPostRating1.setPost(p1);
+	    userPostRating1.setUser(leonardCohen);
+	    userPostRating1.setRating(4.5);
+	    userPostRating1 = appUserPostRatingService.save(userPostRating1);
+	    postService.updateAverageRating(p1);
+
+	    AppUserPostRating userPostRating2 = new AppUserPostRating();
+	    userPostRating2.setPost(p2);
+	    userPostRating2.setUser(johnLennon);
+	    userPostRating2.setRating(4.0);
+	    userPostRating2 = appUserPostRatingService.save(userPostRating2);
+	    postService.updateAverageRating(p2);
+
+	    AppUserPostRating userPostRating3 = new AppUserPostRating();
+	    userPostRating3.setPost(p3);
+	    userPostRating3.setUser(paulMcCartney);
+	    userPostRating3.setRating(5.0);
+	    userPostRating3 = appUserPostRatingService.save(userPostRating3);
+	    postService.updateAverageRating(p3);
+
+	    AppUserPostRating userPostRating4 = new AppUserPostRating();
+	    userPostRating4.setPost(p4);
+	    userPostRating4.setUser(davidBowie);
+	    userPostRating4.setRating(4.2);
+	    userPostRating4 = appUserPostRatingService.save(userPostRating4);
+	    postService.updateAverageRating(p4);
+
+	    AppUserPostRating userPostRating5 = new AppUserPostRating();
+	    userPostRating5.setPost(p5);
+	    userPostRating5.setUser(bobDylan);
+	    userPostRating5.setRating(4.8);
+	    userPostRating5 = appUserPostRatingService.save(userPostRating5);
+	    postService.updateAverageRating(p5);
 	}
+
 }
